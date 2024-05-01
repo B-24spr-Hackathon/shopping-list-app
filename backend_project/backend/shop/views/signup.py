32052dc06801ab237797868c39407c4838d2d86b
canele <canele.yabuuchi@gmail.com>
2024-05-01 17:06:59 +0900

    [fix:back] views/signup.pyの修正

--- backend_project/backend/shop/views/signup.py
+++ backend_project/backend/shop/views/signup.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
-from backend_project.backend.shop.serializers.signup import SignupSerializer
+from shop.serializers.signup import SignupSerializer
 
 
 """
