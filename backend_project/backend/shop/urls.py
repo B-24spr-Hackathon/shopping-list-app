b9a7c429f5f42cee32b890628e1733e20e670568
abyMaya <abycat22@gmail.com>
2024-05-01 17:05:32 +0900

    アイテムリスト表示機能

--- backend_project/backend/shop/urls.py
+++ backend_project/backend/shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from shop.views import items_views
 
 urlpatterns = [
-    path('shop/items/<str:user_id>/<int:list_id>', items_views.ItemList.as_view()),
+    path('shop/items/<user_id>/<list_id>/', items_views.ItemList.as_view()),
+
+
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+
--- backend_project/backend/shop/views/items_views.py
+++ backend_project/backend/shop/views/items_views.py
@@ -5,17 +5,29 @@ from ..serializers.items_serializers import ItemSerializer
 from django.db.models import Q
 
 # Create your views here.
-
+# アイテムリスト（表示）
 class ItemList(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
+    #queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
-# user_idとlist_idで表示する情報をフィルタリング
-# user_idは外部キーであるownerまたはinviteeから取得
+    def get_queryset(self):
+        user_id = self.kwargs.get('user_id')
+        list_id = self.kwargs.get('list_id')
+        # (owner または invitee) かつ list_id__id=list_id の条件でフィルタリング
+        return Item.objects.filter(
+            Q(list_id__owner_id=user_id) | 
+            Q(list_id__members__invitee_id=user_id),
+            list_id=list_id
+        )
+    
+                                   
 
+    
+# アイテムリスト（新規作成）
 
-
+# アイテムリスト（更新）
 class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
+# アイテムリスト（削除）
