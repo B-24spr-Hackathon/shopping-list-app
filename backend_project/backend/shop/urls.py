dcef3ed8ddd44e9df9cff41304adb74fa704d9ca
abyMaya <abycat22@gmail.com>
2024-05-02 21:35:27 +0900

    [fix]ファイル名変更に伴う記述変更

--- backend_project/backend/shop/urls.py
+++ backend_project/backend/shop/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from shop.views import items_views
 from rest_framework.urlpatterns import format_suffix_patterns
-from shop.views import items_views, signup
+from shop.views import items, signup
 
 urlpatterns = [
      # アイテムリスト表示、アイテム作成
-    path('api/items/<user_id>/<list_id>/', items_views.ItemList.as_view()),
+    path('api/items/<user_id>/<list_id>/', items.ItemList.as_view()),
      # アイテム更新
-    path('api/items/<user_id>/<item_id>/', items_views.ItemUpdate.as_view()),
+    path('api/items/<user_id>/<item_id>/', items.ItemUpdate.as_view()),
 ]
 
 urlpatterns += [
