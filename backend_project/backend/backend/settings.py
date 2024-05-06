7ac675016038f90b6105699c12e88a8579f2634c
Kaoru <129600641+kaswister0116@users.noreply.github.com>
2024-05-06 19:06:41 +0900

    signup画面作成 (#17)

--- backend_project/backend/backend/settings.py
+++ backend_project/backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # 'django.contrib.staticfiles',
     "rest_framework",
     "shop",
+    "corsheaders",
 ]
 
 # AbstractUserを使うため追記
@@ -52,11 +53,19 @@ AUTH_USER_MODEL = 'shop.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 ROOT_URLCONF = 'backend.urls'
