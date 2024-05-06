ec8c1d7f16a04f7985506d37dc5bcab879baf1de
canele <canele.yabuuchi@gmail.com>
2024-05-06 16:59:21 +0900

    [fix:back] コンフリクト防止のためにurls.pyを修正

--- backend_project/backend/shop/urls.py
+++ backend_project/backend/shop/urls.py
@@ -3,15 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from shop.views import items_views, user, login, line
 
 urlpatterns = [
-    path('items/<str:user_id>/<int:list_id>', items_views.ItemList.as_view()),
+    path("api/items/<str:user_id>/<int:list_id>", items_views.ItemList.as_view()),
 ]
 
 urlpatterns += [
-    path('user/', user.UserView.as_view(), name='user'),
-    path('login/', login.LoginView.as_view(), name='login'),
-    path('callback/', line.LineCallbackView.as_view(),
-         name='line-callback'),
-    path('line/', line.LineSignupView.as_view(), name='line-signup'),
-    path('line-login/', line.LineLoginView.as_view(), name='line-login')
+    path("api/user/", user.UserView.as_view(), name="user"),
+    path("api/login/", login.LoginView.as_view(), name="login"),
+    path("api/callback/", line.LineCallbackView.as_view(),
+         name="line-callback"),
+    path("api/line/", line.LineSignupView.as_view(), name="line-signup"),
+    path("api/line-login/", line.LineLoginView.as_view(), name="line-login"),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
