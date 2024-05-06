0f34a0f95501be2a7584ef2ac3ea59975ed0e48b
Kaoru <129600641+kaswister0116@users.noreply.github.com>
2024-05-06 19:07:09 +0900

    Revert "フロントからの新規登録テスト (#15)"
    This reverts commit 661f4e6f3953234cb5121dc4c2b89a6ebfa4c743.

--- frontend_project/frontend/react/src/App.jsx
+++ frontend_project/frontend/react/src/App.jsx
@@ -1,33 +1,35 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import List from "./pages/List";
-import Items from "./pages/Items";
-import Setting from "./pages/Setting";
-import Signup from "./pages/Signup";
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
+    <>
       <div>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </Router>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- frontend_project/frontend/react/src/components/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function CertifyBtn({ onClick, children }) {
-    return <button className="" onClick={onClick}>{children}</button>;
-}
-
-
-
-export { CertifyBtn }
