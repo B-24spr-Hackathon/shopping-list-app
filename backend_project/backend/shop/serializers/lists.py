83c813b7df31f1fc0535e8dfaa2a0c95cc733b38
abyMaya <abycat22@gmail.com>
2024-05-07 09:55:51 +0900

    [fix]リストのPOSTに買い物頻度によるバリデーション設定追加

--- backend_project/backend/shop/serializers/lists.py
+++ backend_project/backend/shop/serializers/lists.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from shop.models import  List, Item
+from rest_framework .exceptions import ValidationError
 
 
 # 買い物リスト表示用
@@ -28,6 +29,25 @@ class ListCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
         fields = ('list_name', 'shopping_cycle', 'shopping_day', 'day_of_week',)
+
+    def validate(self, data):
+        # 買い物頻度に基づく要追加入力項目の設定
+        shopping_cycle = data.get('shopping_cycle')
+        shopping_day = data.get('shopping_day')
+        day_of_week = data.get('day_of_week')
+
+        # 買い物頻度が毎月の場合、買い物日要入力
+        if shopping_cycle == 0:
+            if shopping_day is None:
+                raise ValidationError({'shopping_day': 'この項目は入力必須です'})
+        # 買い物頻度が隔週または毎週の場合、買い物曜日要入力   
+        elif shopping_cycle in [1, 2]:
+            if day_of_week is None:
+                raise ValidationError({'day_of_week': 'この項目は入力必須です'})
+            
+            return data
+
+        
 
 # リスト設定（表示）（登録）（更新）(削除)レスポンス用
 class ListResponseSerializer(serializers.ModelSerializer):
