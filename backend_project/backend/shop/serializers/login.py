4dc1e5364057981c640876151f54215df0f064b3
canele <canele.yabuuchi@gmail.com>
2024-05-11 23:50:51 +0900

    [fix:back] views/login.pyの軽微な修正

--- backend_project/backend/shop/serializers/login.py
+++ backend_project/backend/shop/serializers/login.py
@@ -22,7 +22,6 @@ class LoginSerializer(serializers.Serializer):
     def validate(self, data):
         email = data.get("email")
         password = data.get("password")
-        print(email, password)
         if email and password:
             user = authenticate(username=email, password=password)
             if user:
