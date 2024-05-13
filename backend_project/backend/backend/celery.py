a47ca97a766128e9dc36c17b9a1c2b6ed94782f6
canele <canele.yabuuchi@gmail.com>
2024-05-13 22:40:08 +0900

    [add:back] celery用の設定ファイル追加

--- backend_project/backend/backend/celery.py
+++ backend_project/backend/backend/celery.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Djangoの設定ファイルを指定
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-app = Celery("message")
+app = Celery("backend")
 
 # celeryの設定ソースとしてDjangoのsettingsモジュールを追加
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -14,9 +14,13 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # Celery Beatによるバッチ処理のスケジュール
 app.conf.beat_schedule = {
     "shopping-batch": {
-        "task": "shop.message.shopping_batch",
-        "schedule": crontab(hour=settings.BATCH_TIME),
-    }
+        "task": "shop.tasks.shopping_batch",
+        "schedule": crontab(hour=(settings.BATCH_TIME), minute=0),
+    },
+    "remind-batch": {
+        "task": "shop.tasks.remind_batch",
+        "schedule": crontab(hour=settings.BATCH_TIME, minute=0),
+    },
 }
 
 # 登録されているDjangoアプリからタスクモジュールを読込み
