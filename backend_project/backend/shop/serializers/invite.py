b406582d33a19481d98989942ae8aed979f65599
abyMaya <abycat22@gmail.com>
2024-05-14 17:24:30 +0900

    作業中

--- backend_project/backend/shop/serializers/invite.py
+++ backend_project/backend/shop/serializers/invite.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from shop.models import  User
 
 
-# 招待者情報取得 
+# 招待者情報取得用
 class FindUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ('user_id', 'user_name', 'user_icon')
+
+# 招待送信リクエスト用
+
+# 招待送信レスポンス用
+
+
         
 
 
