907391d459e4523e6381d8b11eed94169bee95a8
abyMaya <abycat22@gmail.com>
2024-05-15 20:06:24 +0900

    [fix]have_list処理追記

--- backend_project/backend/shop/views/lists.py
+++ backend_project/backend/shop/views/lists.py
@@ -90,5 +90,17 @@ class ListView(APIView):
         serialized_data = response_serializer.data
 
         list_instance.delete()
+        
+        # 他のリストに登録されているか確認
+        owner_lists_count = List.objects.filter(owner_id=request.user).count()
+        guest_lists_count = Member.objects.filter(guest_id=request.user, member_status=0).count()
+        print(owner_lists_count)
+        print(guest_lists_count)
+        other_lists_count = owner_lists_count + guest_lists_count
+        # have_listを更新
+        if other_lists_count == 0:
+            request.user.have_list = False
+        print(request.user.have_list)
+        request.user.save()
 
         return Response(serialized_data, status=status.HTTP_200_OK)
