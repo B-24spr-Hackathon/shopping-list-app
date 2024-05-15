58ae88aef722820e8832ae262827fda942aaf64f
abyMaya <abycat22@gmail.com>
2024-05-15 19:35:49 +0900

    [fix]ownerlistのcount成功

--- backend_project/backend/shop/views/entry.py
+++ backend_project/backend/shop/views/entry.py
@@ -8,6 +8,7 @@ from shop.authentication import CustomJWTAuthentication
 from shop.serializers.invite import FindUserSerializer
 from django.shortcuts import get_object_or_404
 from shop.permissions import IsOwner
+from django.db.models import Q
 
 class EntryView(APIView):
     # JWT認証を要求、オーナーのみ許可
@@ -33,7 +34,6 @@ class EntryView(APIView):
 
     # 招待・申請機能　共有解除 DELETE
     def delete(self, request, member_id):
-        print(member_id)
         
         # ゲスト、リストを取得
         guest = get_object_or_404(Member, pk=member_id)
@@ -42,7 +42,21 @@ class EntryView(APIView):
         self.check_object_permissions(self.request, list_instance)
         # ユーザー名を取得
         user_name = guest.guest_id.user_name
+        # ゲストを削除
         guest.delete()
+
+        # 削除されたゲストが他のリストに登録されているか確認
+        owner_lists_count = List.objects.filter(owner_id=guest.guest_id).count()
+        guest_lists_count = Member.objects.filter(guest_id=guest.guest_id, member_status=0).count()
+        print(owner_lists_count)
+        print(guest_lists_count)
+        other_lists_count = owner_lists_count + guest_lists_count
+        # have_listを更新
+        if other_lists_count == 0:
+            guest.guest_id.have_list = False
+
+        guest.guest_id.save()
+
         # レスポンス用データ
         data = {
             'user_name': user_name,
