48d25253d4751796595c5787d1d7f87b180ac547
canele <canele.yabuuchi@gmail.com>
2024-05-16 19:38:04 +0900

    [fix:back] command.shにログファイル用ディレクトリ作成コマンド追加

--- backend_project/backend/command.sh
+++ backend_project/backend/command.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# ログ出力用のディレクトリが存在しない場合は作成する
+name="/backend/logs"
+if [ ! -d "$name" ]; then
+    mkdir "$name"
+fi
+
 python /backend/manage.py makemigrations
 python /backend/manage.py migrate
 celery -A backend worker -l info &
